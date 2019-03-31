4dc940842a3ed9047e9328453bbf05f28281ef2a
edward <edwardgsl5381@gmail.com>
2019-03-31 16:41:01 +0800

    edward

--- web/public/js/public-load-image.js
+++ web/public/js/public-load-image.js
@@ -1,64 +1,57 @@
 "use strict";
 
-// const config = require('../../../config');
-// const async = require('async');
-// const cloudinary = require('cloudinary');
+const config = require('../../../config');
+const async = require('async');
+const cloudinary = require('cloudinary');
 
-// const ProductMaster = require('../../../api/v1.0/model/product-master');
+const ProductMaster = require('../../../api/v1.0/model/product-master');
 
-//  module.exports = function(req, res) {
+ module.exports = function(req, res) {
 	window.onload = function() {
-		alert("here")
-		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
+		// alert("here")
+		// var fileInput = document.getElementById('fileInput');
+		// var fileDisplayArea = document.getElementById('fileDisplayArea');
+		// fileInput.addEventListener('change', function(e) {
+		// 	var file = fileInput.files[0];
+		// 	var imageType = /image.*/;
 
+		// 	if (file.type.match(imageType)) {
+		// 		var reader = new FileReader();
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			var imageType = /image.*/;
+		// 		reader.onload = function(e) {
+		// 			fileDisplayArea.innerHTML = "";
 
-			if (file.type.match(imageType)) {
-				var reader = new FileReader();
+		// 			var img = new Image();
+		// 			img.src = reader.result;
 
-				reader.onload = function(e) {
-					fileDisplayArea.innerHTML = "";
+		// 			fileDisplayArea.appendChild(img);
+		// 		}
 
-					var img = new Image();
-					img.src = reader.result;
+		// 		reader.readAsDataURL(file);	
+		// 	} else {
+		// 		fileDisplayArea.innerHTML = "File not supported!"
+		// 	}
+		// });
+	}
 
-					fileDisplayArea.appendChild(img);
-				}
-
-				reader.readAsDataURL(file);	
-			} else {
-				fileDisplayArea.innerHTML = "File not supported!"
+		ProductMaster.find({ 'image': image }, function(err, result) {
+			if (err) {
+				error.push(config.getErrorResponse('', 501));
+				let resp = config.getResponse(res, 500, error, {}, err);
+				config.logApiCall(req, res, resp);
+				return;
 			}
+			if (result) {
+					result = config.getImage(result);
+					req.session['publicImage'] = result['image'];
+					let resp = config.getResponse(res, 100, error, { 'image_info': result });
+					config.logApiCall(req, res, resp);
+					return;
+			}
+			error.push(config.getErrorResponse(config.API['VERIFY_ADMIN_LOGIN'], 203));
+			let resp = config.getResponse(res, 200, error, {});
+			config.logApiCall(req, res, resp);
 		});
 
 }
-
-
-	
-		// ProductMaster.find({ 'image': image }, function(err, result) {
-		// 	if (err) {
-		// 		error.push(config.getErrorResponse('', 501));
-		// 		let resp = config.getResponse(res, 500, error, {}, err);
-		// 		config.logApiCall(req, res, resp);
-		// 		return;
-		// 	}
-		// 	if (result) {
-		// 			result = config.getImage(result);
-		// 			req.session['publicImage'] = result['image'];
-		// 			let resp = config.getResponse(res, 100, error, { 'image_info': result });
-		// 			config.logApiCall(req, res, resp);
-		// 			return;
-		// 	}
-		// 	error.push(config.getErrorResponse(config.API['VERIFY_ADMIN_LOGIN'], 203));
-		// 	let resp = config.getResponse(res, 200, error, {});
-		// 	config.logApiCall(req, res, resp);
-		// });
-	
-
-
-// }
 
