6a630d22f795f4d1e27efbc5a037674f9398298f
tsaiboon <tansaiboom2@hotmail.com>
2019-05-04 18:01:47 +0800

    [PUBLIC] add order api (incomplete)

---
+++ api/v1.0/model/order.js
@@ -0,0 +1,32 @@
+const config = require('./../../../config');
+const mongoose = require('mongoose');
+
+const addressSchema = mongoose.Schema({
+    first_line: String,
+    second_line: String,
+    third_line: String,
+    city: String,
+    postcode: String,
+    state: String,
+    country: String
+});
+
+const schema = mongoose.Schema({
+    id: String,
+    email_addr: String,
+    status: String,
+    image_ic: String,
+    image_card: String,
+    image_signature: String,
+    phone_no: String,
+    emergency_no: String,
+    address: addressSchema,
+    order_product: Array,
+    order_date: { type: Number, default: config.getCurrentTimestamp() },
+    remarks: String,
+    opr_by: String,
+    opr_date: { type: Number, default: config.getCurrentTimestamp() },
+    opr_func: String
+    
+});
+module.exports = mongoose.model('Order', schema, config.DB['TBL_ORDER']);
