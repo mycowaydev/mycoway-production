4fa4883951ee26a567bbbea2530fa29b97f741fb
tsaiboon <tansaiboom2@hotmail.com>
2019-05-30 00:34:08 +0800

    [PUBLIC] update new FAQ with language option

--- assets/public/js/lang/lang.js
+++ assets/public/js/lang/lang.js
@@ -1,30 +1,25 @@
-function changeLanguage(lang) {
+function getArray(lang){
     if (lang){
-        $('#selected_language').removeClass('flag-icon-gb')
-        $('#selected_language').removeClass('flag-icon-cn')
-        $('#selected_language').removeClass('flag-icon-my')
-        if (lang == "en"){
-            sessionStorage.setItem("language", "en");
-            lang_array = language_en
-            $('#selected_language').addClass('flag-icon-gb')
+        if (lang == "my"){
+            return language_my
         } else if (lang == "cn") {
-            sessionStorage.setItem("language", "cn");
-            lang_array = language_cn
-            $('#selected_language').addClass('flag-icon-cn')
+            return language_cn
         } else {
-            sessionStorage.setItem("language", "my");
-            lang_array = language_my
-            $('#selected_language').addClass('flag-icon-my')
+            return language_en
         }
-        for (k in lang_array) {
-            element = $('.'+k)
-            if (lang_array[k]){
-                if (element){
-                    if (k.startsWith("string_") || k.startsWith("validation_") || k.startsWith("alert_")){
-                        element.html(lang_array[k])
-                    } else if (k.startsWith("placeholder_")){
-                        element.attr("placeholder", lang_array[k]);
-                    }
+    }
+    return language_en
+}
+
+function showStringByArray(lang_array){
+    for (k in lang_array) {
+        element = $('.'+k)
+        if (lang_array[k]){
+            if (element){
+                if (k.startsWith("string_") || k.startsWith("validation_") || k.startsWith("alert_")){
+                    element.html(lang_array[k])
+                } else if (k.startsWith("placeholder_")){
+                    element.attr("placeholder", lang_array[k]);
                 }
             }
         }
@@ -36,13 +31,41 @@ function showString(){
     if (window.sessionStorage.getItem("language") != null){
         lang = window.sessionStorage.getItem("language")
     }
-    changeLanguage(lang)
+    showStringByArray(getArray(lang))
+}
+
+function refreshFaq(){
+     if (typeof selected_tab !== 'undefined') {
+         if (selected_tab == 'tab-faq') {
+             document.location.reload();
+         }
+     }
+ }
+
+function changeLanguage(lang) {
+    if (lang){
+        $('#selected_language').removeClass('flag-icon-gb')
+        $('#selected_language').removeClass('flag-icon-cn')
+        $('#selected_language').removeClass('flag-icon-my')
+        if (lang == "my"){
+            sessionStorage.setItem("language", "my");
+            $('#selected_language').addClass('flag-icon-my')
+        } else if (lang == "cn") {
+            sessionStorage.setItem("language", "cn");
+            $('#selected_language').addClass('flag-icon-cn')
+        } else {
+            sessionStorage.setItem("language", "en");
+            $('#selected_language').addClass('flag-icon-gb')
+        }
+    }
+    showString()
+    refreshFaq()
+}
+
+function toggleNav(){
+    $('#nav-toggler-button-min').trigger('click');
 }
 
 $(document).ready(function(){
     showString();
 });
-
-function toggleNav(){
-    $('#nav-toggler-button-min').trigger('click');
-}
