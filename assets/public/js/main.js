dbbb9a63041486a714b47e70df8a6380a9af56ff
tsaiboon <tansaiboom2@hotmail.com>
2020-01-08 23:41:17 +0800

    Fix review date

--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -58,7 +58,7 @@ function getFormattedNumber(num, slider) {
 
 function getDateFormattedString(epouch) {
     var convertedDate = new Date(epouch * 1000);
-    return convertedDate.getFullYear() + "/" + getFormattedNumber(convertedDate.getMonth(), -2) + "/" + getFormattedNumber(convertedDate.getDate(), -2)
+    return convertedDate.getFullYear() + "/" + getFormattedNumber(convertedDate.getMonth()+1, -2) + "/" + getFormattedNumber(convertedDate.getDate(), -2)
         + " " + getFormattedNumber(convertedDate.getHours(), -2) + ":" + getFormattedNumber(convertedDate.getMinutes(), -2);
 }
 
